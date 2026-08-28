778a9b711609a089faca83f64a9ca63cad854c52
srinadh <srinadhkonduri@gmail.com>
2026-08-28 12:24:04 +0530

    final

--- src/main/java/com/moneymanager/config/SecurityConfig.java
+++ src/main/java/com/moneymanager/config/SecurityConfig.java
@@ -41,9 +41,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.requestMatchers(
                                 "/status",
                                 "/health",
-                                "/api/v1.0/register",
-                                "/api/v1.0/activate",
-                                "/api/v1.0/login",
+                                "/register",
+                                "/activate",
+                                "/login",
                                 "/test",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**").permitAll()
